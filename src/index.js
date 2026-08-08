7b8fbe7b49eb29fef499638f1441fa266375b673
Daniyal <daniyalafzaal@yahoo.com>
2026-08-08 20:20:13 +0500

    connectDB handling, and starting server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app,js";
 
-dotenv.config({
+dotenv.config({      // Load environment variables from the .env file into process.env
     path: './.env'
 })
 
-connectDB();
+const PORT = process.env.PORT || 8000;
+
+connectDB()  //b/c every async method declared with async returns a promise so we will handle the result of promise in .then and .catch
+.then(()=>{
+    const server = app.listen(PORT , ()=>{          // app.listen() starts the HTTP server and returns the server instance
+        console.log(`App is listening on PORT ${PORT}`);        
+    })
+
+    server.on("error", (err) => {          //if any error occurs like port occupied
+        console.error("Server Error : ", err);
+        process.exit(1);
+    })
+})
+.catch( (err) => {
+    console.error("MongoDB Connection Failed:", err);
+    process.exit(1);
+});
 
 
 
