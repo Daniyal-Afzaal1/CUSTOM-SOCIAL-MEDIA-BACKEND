f8c37fcf94df6447b65645b2871cd9e4604bd81d
Daniyal <daniyalafzaal@yahoo.com>
2026-08-16 09:37:58 +0500

    add refresh token route to user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"; //if we write in {}, then we have to write the exact name exported
-import { LoginUser, LogoutUser, registerUser } from "../controllers/user.controllers.js";
+import { LoginUser, LogoutUser, refreshAccessToken, registerUser } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
@@ -21,5 +21,7 @@ router.route("/register").post( //register
 router.route("/login").post(LoginUser);  //login
 
 router.route("/logout").post(verifyJWT, LogoutUser); //logout
+
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router; //with default in export, in import we can give it any name we want
