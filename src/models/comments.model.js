b355ec1282d43fe40ee5f5801624b95a2af5bcb2
Daniyal <daniyalafzaal@yahoo.com>
2026-08-19 08:36:39 +0500

    feat: add models for comments, likes, playlists, and tweets with necessary schemas

---
+++ src/models/comments.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const commentsSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    video: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+        required: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+}, { timestamps: true })
+
+videoModel.plugin(mongooseAggregatePaginate); //before export so model can have the plugin abilities to perform aggregation pipeline operations        
+
+export const Comment = mongoose.model("Comment", commentsSchema);
---
+++ src/models/playlists.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const playlistsSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    videos: {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "Video",
+            required : true
+        },
+    owner: {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User",
+            required : true
+        },
+    
+
+},{timestamps : true})
+
+export const Playlist = mongoose.model("Playlist",playlistsSchema);
